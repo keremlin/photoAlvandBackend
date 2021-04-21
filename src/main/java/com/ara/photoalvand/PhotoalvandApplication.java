57618b2f9ec5815921db487b0a0e8744f71e2c9c
arash yeganeh rad <arash.yegane@gmail.com>
2021-04-21 15:05:18 +0430

    add file upload ability

--- src/main/java/com/ara/photoalvand/PhotoalvandApplication.java
+++ src/main/java/com/ara/photoalvand/PhotoalvandApplication.java
@@ -1,13 +1,25 @@
 package com.ara.photoalvand;
 
+import javax.annotation.Resource;
+
+import com.ara.photoalvand.services.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PhotoalvandApplication {
-
+	@Resource
+	FilesStorageService storageService;
+  
 	public static void main(String[] args) {
-		SpringApplication.run(PhotoalvandApplication.class, args);
+	  SpringApplication.run(PhotoalvandApplication.class, args);
+	}
+  
+	@Override
+	public void run(String... arg) throws Exception {
+	  storageService.deleteAll();
+	  storageService.init();
 	}
 
 }
