6b0820078885f150ad4eda4d5ef9024ffd31b598
keremlin <arash.yeganehrad@gmail.com>
2022-04-28 12:20:42 +0430

    Start bean validation

--- src/main/java/com/ara/photoalvand/controllers/categoryController.java
+++ src/main/java/com/ara/photoalvand/controllers/categoryController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/category")
+@Validated
 public class categoryController {
 	
 	@Autowired 
--- src/main/java/com/ara/photoalvand/exception/HandleException.java
+++ src/main/java/com/ara/photoalvand/exception/HandleException.java
@@ -1,9 +1,14 @@
 package com.ara.photoalvand.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.validation.FieldError;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,5 +35,19 @@ public class HandleException {
         log.error(ex.getMessage());
         return ex.getMessage();
     }
+    
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
 
 }
